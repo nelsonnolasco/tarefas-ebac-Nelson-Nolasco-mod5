3d987e3a740ec337b48a42918fe049f651bad43c
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-01-21 16:03:39 -0300

    Add files via upload

---
+++ PrimeiroProjeto/src/br/com/nelsonnolasco/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.nelsonnolasco;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Nelson Nolasco");
+	}
+
+}
---
+++ PrimeiroProjeto/tarefas-ebac-Nelson-Nolasco-mod5/README.md
@@ -0,0 +1,2 @@
+# tarefas-ebac-Nelson-Nolasco-mod5-PrimeiroProjeto
+Trata-se do exercício do módulo 5 backend Java do EBAC.
